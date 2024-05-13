71a68b69f0980c9fe69cdb43b77079a99487ba63
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-05-13 22:12:06 +0200

    les images svg sont au bon endroit, plus qu'a les tailler en sass

--- src/components/Techno.jsx
+++ src/components/Techno.jsx
@@ -1,15 +1,48 @@
+import html from "../assets/Svg/html-5.svg";
+import css from "../assets/Svg/css-3.svg";
+import javaScript from "../assets/Svg/js.svg";
+import reactSvg from "../assets/Svg/react.svg";
+import sass from "../assets/Svg/sass.svg";
+import threeJs from "../assets/Svg/three-js.svg";
+import git from "../assets/Svg/git.svg";
+import gitHub from "../assets/Svg/github.svg";
+
 function Techno() {
   return (
     <>
       <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Sass</li>
-        <li>Three.js</li>
-        <li>Git</li>
-        <li>GitHub</li>
+        <li>
+          <img src={html} className="svgImg" alt="logo svg de HTML" />
+          HTML
+        </li>
+        <li>
+          <img src={css} className="svgImg" alt="logo svg de CSS" />
+          CSS
+        </li>
+        <li>
+          <img src={javaScript} className="svgImg" alt="logo svg de JS" />
+          JavaScript
+        </li>
+        <li>
+          <img src={reactSvg} className="svgImg" alt="logo svg de React" />
+          React
+        </li>
+        <li>
+          <img src={sass} className="svgImg" alt="logo svg de Sass" />
+          Sass
+        </li>
+        <li>
+          <img src={threeJs} className="svgImg" alt="logo svg de three.js" />
+          Three.js
+        </li>
+        <li>
+          <img src={git} className="svgImg" alt="logo svg de git" />
+          Git
+        </li>
+        <li>
+          <img src={gitHub} className="svgImg" alt="logo svg de GitHub" />
+          GitHub
+        </li>
       </ul>
     </>
   );
