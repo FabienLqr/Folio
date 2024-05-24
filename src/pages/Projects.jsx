8192690b5cba68476023f372d0b57ecd235b6911
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-05-24 21:20:51 +0200

    debut de travail sur la section project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,14 @@
 function Projects() {
-  return <p>COUCOU</p>;
+  return (
+    <>
+      <h2>Mes Projets</h2>
+      <p>
+        Au cour de ma formation a la Wild Code School, on a eu plusieurs projet
+        a faire qui vont etre liste ci dessous. Cependant des projets perso
+        seront aussi presente dans cette section
+      </p>
+    </>
+  );
 }
 
 export default Projects;
