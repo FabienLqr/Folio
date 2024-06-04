c04ff6f58462bed51908befc49ecba4590496121
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-04 18:58:38 +0200

    updte dataProjects.jsx

--- src/data/dataProjects.js
+++ src/data/dataProjects.js
@@ -1,5 +1,9 @@
 import DragonFlyImg from "../assets/Imgages/DragonFly.png";
 import MapImg from "../assets/Imgages/Map.png";
+import StarWildImg from "../assets/Imgages/StarWild.png";
+import ECFImg from "../assets/Imgages/ecf.png";
+
+import loadImg from "../assets/Imgages/load.jpg";
 
 const projects = [
   {
@@ -11,28 +15,28 @@ const projects = [
   },
   {
     name: "Project 2: Star-Wild",
-    img: "",
+    img: StarWildImg,
     desc: "Star-Wild est un site a des vus pedagogiques sur le system solaire utilisant une API",
     repo: "https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-CrewDragon-P2-Team3",
     deploy: "https://star-wild.netlify.app/",
   },
   {
     name: "Project 3: Street Art Hunter",
-    img: "",
+    img: loadImg,
     desc: "Sreet Art Hunter est un site qui a pour objectif de faire bouger les jeunes qui sont trop sur leur telephone en leur faisant decouvrir l'art a travers le stret art.",
     repo: "https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-CrewDragon-P3-StreetArtHunter",
     deploy: "",
   },
   {
     name: "Project 4: Protojam",
-    img: "",
+    img: ECFImg,
     desc: "Le protojam appele echappee celebrement fantasque est un projet repondant au sujet nature est deconnexion qui propose des voyages dans la nature avec une celibrite",
     repo: "https://github.com/WildCodeSchool-2024-02/wcs-Protojam-goupeA",
     deploy: "https://echappee-celebrement-fantasque.netlify.app/",
   },
   {
     name: "Project 5: Metallica",
-    img: "",
+    img: loadImg,
     desc: "Le projet Metallica est un projet qui sert a regrouper differentes informations sur le groupe, dont les paroles des chansons ect.",
     repo: "",
     deploy: "",
