2a24f1f2e430fa86d35fa14cec59bb81a6fda38a
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-04 18:37:49 +0200

    add info in dataProjects, continue the Projects.jsx view and set scss

--- src/data/dataProjects.js
+++ src/data/dataProjects.js
@@ -1,26 +1,38 @@
+import DragonFlyImg from "../assets/Imgages/DragonFly.png";
+
 const projects = [
   {
-    name: "Project 1: Horloge",
+    name: "Project 1: DragonFly",
+    img: DragonFlyImg,
+    desc: "DragonFly est un site qui sert a afficher l'heure de plusieurs villes du monde en simultane.",
     repo: "https://github.com/VncPsq/DragonFly",
     deploy: "https://vncpsq.github.io/DragonFly/landing/",
   },
   {
     name: "Project 2: Star-Wild",
+    img: "",
+    desc: "Star-Wild est un site a des vus pedagogiques sur le system solaire utilisant une API",
     repo: "https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-CrewDragon-P2-Team3",
     deploy: "https://star-wild.netlify.app/",
   },
   {
     name: "Project 3: Street Art Hunter",
+    img: "",
+    desc: "Sreet Art Hunter est un site qui a pour objectif de faire bouger les jeunes qui sont trop sur leur telephone en leur faisant decouvrir l'art a travers le stret art.",
     repo: "https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-CrewDragon-P3-StreetArtHunter",
     deploy: "",
   },
   {
     name: "Project 4: Protojam",
+    img: "",
+    desc: "Le protojam appele echappee celebrement fantasque est un projet repondant au sujet nature est deconnexion qui propose des voyages dans la nature avec une celibrite",
     repo: "https://github.com/WildCodeSchool-2024-02/wcs-Protojam-goupeA",
     deploy: "https://echappee-celebrement-fantasque.netlify.app/",
   },
   {
     name: "Projects 5: Metallica",
+    img: "",
+    desc: "Le projet Metallica est un projet qui sert a regrouper differentes informations sur le groupe, dont les paroles des chansons ect.",
     repo: "",
     deploy: "",
   },
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,6 +6,13 @@ function Projects() {
       {projects.map((project) => (
         <section key={project.name} className="project">
           <h2>{project.name}</h2>
+          <article>
+            <img src={project.img} />
+            <p>{project.desc}</p>
+            <span>
+              <button>REPO</button> <button>DEPLOY</button>
+            </span>
+          </article>
         </section>
       ))}
     </section>
