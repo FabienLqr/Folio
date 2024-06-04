4ccd599c0404f5ac8e7abd191ef94791198f4e42
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-04 18:12:41 +0200

    Projects.jsx - array.map() done

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,13 @@
+import projects from "../data/dataProjects";
+
 function Projects() {
   return (
     <>
-      <h2>Mes Projets</h2>
-      <p>
-        Au cour de ma formation a la Wild Code School, on a eu plusieurs projet
-        a faire qui vont etre liste ci dessous. Cependant des projets perso
-        seront aussi presente dans cette section
-      </p>
+      {projects.map((project) => (
+        <section key={project.name}>
+          <h2>{project.name}</h2>
+        </section>
+      ))}
     </>
   );
 }
