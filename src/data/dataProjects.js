5f19a62c560a740e979aaadd0483e36886e067ef
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-04 17:35:44 +0200

    set dataProjects template and write some datas

---
+++ src/data/dataProjects.js
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    name: "Project 1: Horloge",
+    repo: "https://github.com/VncPsq/DragonFly",
+    deploy: "https://vncpsq.github.io/DragonFly/landing/",
+  },
+  {
+    name: "Project 2: Star-Wild",
+    repo: "https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-CrewDragon-P2-Team3",
+    deploy: "https://star-wild.netlify.app/",
+  },
+  {
+    name: "Project 3: Street Art Hunter",
+    repo: "https://github.com/WildCodeSchool-2024-02/JS-RemoteFR-CrewDragon-P3-StreetArtHunter",
+    deploy: "",
+  },
+  {
+    name: "Project 4: Protojam",
+    repo: "https://github.com/WildCodeSchool-2024-02/wcs-Protojam-goupeA",
+    deploy: "https://echappee-celebrement-fantasque.netlify.app/",
+  },
+  {
+    name: "Projects 5: Metallica",
+    repo: "",
+    deploy: "",
+  },
+];
+
+export default projects;
