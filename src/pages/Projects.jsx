c931098f1ffce4ae854e0013f9010730f4908ec8
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-04 18:15:59 +0200

    add some commentary

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,13 +2,13 @@ import projects from "../data/dataProjects";
 
 function Projects() {
   return (
-    <>
+    <section className="projects">
       {projects.map((project) => (
-        <section key={project.name}>
+        <section key={project.name} className="project">
           <h2>{project.name}</h2>
         </section>
       ))}
-    </>
+    </section>
   );
 }
 
